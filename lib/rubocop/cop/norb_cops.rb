cd23a6e6569074260c750d9a73bd36e635f07984
IharBykouski <igor.bykouski@cleverlabs.io>
2026-03-27 16:48:16 +0300

    remove outdated TargetRailsVersion override

--- lib/rubocop/cop/norb_cops.rb
+++ lib/rubocop/cop/norb_cops.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'mixin/target_rails_version'
-
 require_relative 'norb/active_record_namespaced'
 require_relative 'norb/active_record_through_business'
 require_relative 'norb/branching_logic'
